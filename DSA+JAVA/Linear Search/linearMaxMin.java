92ee94db956f33f69b71bb6e7d682e583dd2f9a4
AgrahariBipin <138454644+AgrahariBipin@users.noreply.github.com>
2023-08-10 11:52:28 +0530

    Update linearMaxMin.java

--- DSA+JAVA/Linear Search/linearMaxMin.java
+++ DSA+JAVA/Linear Search/linearMaxMin.java
@@ -1,5 +1,3 @@
-
-
 class linearMaxMin{
     public static void main(String[] args){
         int[] arr={1,4,2,6,11,90,0};
@@ -8,10 +6,11 @@ class linearMaxMin{
         System.out.println("Minimum :"+min(arr));
     }
 
+    //return the maximum element in array
     static int max(int[] arr){
         int maxV=0;
         if(arr.length==0){
-           return Integer.MAX_VALUE;
+           return Integer.MAX_VALUE;   
         }
         for(int i=0;i<arr.length;i++){
             if(arr[i]>maxV){
@@ -23,6 +22,7 @@ class linearMaxMin{
         return maxV;
     }
 
+    //return the minimum element inthe aray
     static int min(int[] arr){
          int minV=0;
         if(arr.length==0){
